a2f1950ba6141652b0665b9987238d64bd6c6fdc
Team-4-prog <60031881+LukaAbramovic775@users.noreply.github.com>
2021-05-10 14:41:07 +0200

    commit on main

--- src/test/trigramGenerator.spec.js
+++ src/test/trigramGenerator.spec.js
@@ -12,4 +12,23 @@ describe("trigram generator problem", function () {
     it("function should throw error if argument is not a text", function (){
       expect(()=> trigramGenerator(1)).to.throw(Error);
     });
+    it('should throw error if argument is not a string', function () {
+      expect(() => trigramGenerator(5)).to.throw();
+      expect(() => trigramGenerator([])).to.throw();
+      expect(() => trigramGenerator({})).to.throw();
+    });
+
+  it('should return an object', function () {
+    // expect(trigramGenerator('')).to.be.an('object');
+    expect(typeof trigramGenerator('')).to.equal('object');
+  });
+
+  it(`should return correct trigram for 2 words input`, function () {
+    expect(trigramGenerator('I wish')).to.eql(new Map());
+  });
+
+  it(`should return correct trigram for 3 words input`, function () {
+    console.log(trigramGenerator('I wish I'));
+    expect(trigramGenerator('I wish I')).to.eql(new Map([['I wish', ['I']]]));
+  });
 });
